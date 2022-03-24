121a9c2fb9c4fc74ddb02f10f2cc4e77baa66680
crazywoola <ufozhengli@163.com>
2022-03-24 17:18:55 +0800

    Add: 🚀

---
+++ workers-site/index.js
@@ -0,0 +1,18 @@
+import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
+
+// eslint-disable-next-line no-restricted-globals
+addEventListener("fetch", event => {
+  event.respondWith(handleEvent(event));
+});
+
+async function handleEvent(event) {
+  try {
+    return await getAssetFromKV(event);
+  } catch (e) {
+    let pathname = new URL(event.request.url).pathname;
+    return new Response(`"${pathname}" not found`, {
+      status: 404,
+      statusText: "not found"
+    });
+  }
+}
