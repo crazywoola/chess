50eb10ef3e66e291bfd0579edceaa7f906a6e320
crazywoola <ufozhengli@163.com>
2022-03-25 13:47:43 +0800

    Add: board context

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import 'src/App.css';
 import SvgBoard from 'src/components/svg-board';
 import Board from 'src/components/board';
+import BoardContextProvider from 'src/context/board';
+
 function App() {
     return (
         <div className="app">
-            <div className='container'>
-                <Board />
-            </div>
-            <div className='container'>
-                <SvgBoard />
-            </div>
+            <BoardContextProvider>
+                <div className='container'>
+                    <Board />
+                </div>
+                <div className='container'>
+                    <SvgBoard />
+                </div>
+            </BoardContextProvider>
         </div>
     );
 }
