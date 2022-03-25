473d4af8622408986e459570a75d5e706efa7093
crazywoola <427733928@qq.com>
2022-03-25 15:59:51 +0800

    Add: action select

---
+++ src/components/action-select/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const ActionSelect = () => {
+    return <div className='action-select'>
+        <h3>Select Action</h3>
+        <div className={'actions'}>
+            <button >Reset Chessboard</button>
+        </div>
+    </div>
+};
+
+export default ActionSelect;
--- src/context/board/index.tsx
+++ src/context/board/index.tsx
@@ -2,14 +2,16 @@ import React, { FC, createContext, useState } from 'react';
 import { initMap } from 'src/constant';
 interface BoardContextProps {
     chessboard: string[][];
+    resetChessboard: () => void;
 }
 export const BoardContext = createContext<BoardContextProps>({} as BoardContextProps);
 
 const BoardContenxtProvider: FC = ({ children }) => {
-    const [chessboard, ] = useState(initMap);
+    const [chessboard, setChessboard] = useState(initMap);
     return <BoardContext.Provider
         value={{ 
-            chessboard
+            chessboard,
+            resetChessboard: () => setChessboard(initMap)
         }}
     >
         {children}
