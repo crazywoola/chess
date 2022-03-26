150f2219d2a7552c877ae247e523776abcc130ea
banana <banana@eurus.cn>
2022-03-26 18:12:55 +0800

    Add: hisotry book

--- src/components/chess-manual/index.tsx
+++ src/components/chess-manual/index.tsx
@@ -1,43 +1,18 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { BoardContext } from 'src/context/board';
+import chunk from 'lodash/chunk';
 const ChessManual = () => {
+    const { chessboard } = useContext(BoardContext);
     return <article className='article'>
         <h4 className=''>Chess Manual</h4>
         <p className="article-meta">Written by Banana</p>
         <p className="lead">Still Work In Progress</p>
         <div className="row">
             <div className="col padding-none">
-                <p>White Moves</p>
                 <ol>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
-                </ol>
-            </div>
-            <div className="col padding-none">
-                <p>Black Moves</p>
-                <ol>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
-                    <li> Pawn to A3</li>
-                    <li> Pawn to C4</li>
-                    <li> Knight to F3</li>
+                    {chunk(chessboard.history(), 2).map((i: any, idx: number) => {
+                        return <li key={idx}>{i[0]} {i[1]}</li>
+                    })}
                 </ol>
             </div>
         </div>
