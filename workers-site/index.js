1d07ff0c152475319a15edb78ba8313eba6c5733
banana <banana@eurus.cn>
2022-03-27 09:36:32 +0800

    feat: add some workers test function

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,18 +1,36 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-
-// eslint-disable-next-line no-restricted-globals
-addEventListener("fetch", event => {
-  event.respondWith(handleEvent(event));
-});
+/* eslint-disable no-restricted-globals */
+import {
+    getAssetFromKV
+} from "@cloudflare/kv-asset-handler";
 
 async function handleEvent(event) {
-  try {
-    return await getAssetFromKV(event);
-  } catch (e) {
-    let pathname = new URL(event.request.url).pathname;
-    return new Response(`"${pathname}" not found`, {
-      status: 404,
-      statusText: "not found"
-    });
-  }
+    const {
+        request
+    } = event;
+    try {
+        return await getAssetFromKV(event);
+    } catch (e) {
+        let pathname = new URL(event.request.url).pathname;
+        if (pathname === '/export') {
+            if (request.method === 'POST') {
+                const incoming = JSON.stringify(await request.json())
+                return new Response(incoming, {
+                    headers: {
+                        'content-type': 'application/json;charset=UTF-8',
+                    },
+                })
+            } else if (request.method === 'GET') {
+                return new Response(`GET`);
+            }
+        } else {
+            return new Response(`"${pathname}" not found`, {
+                status: 404,
+                statusText: "not found"
+            });
+        }
+    }
 }
+
+addEventListener("fetch", event => {
+    event.respondWith(handleEvent(event));
+});
