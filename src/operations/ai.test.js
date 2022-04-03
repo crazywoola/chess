8f3598fde4e55cace445bc9bbdc4d9c26f5e9ab8
banana <banana@eurus.cn>
2022-04-03 10:56:47 +0800

    feat: add minmax with ab

--- src/operations/ai.test.js
+++ src/operations/ai.test.js
@@ -1,6 +1,24 @@
-import { Node } from './ai';
+import {
+    Node,
+    NodeWithPruning,
+} from './ai';
 
-test('get nodes', () => {
+test('evaluate boards', () => {
     const node = new Node(4, 0, 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', 'max');
-    node.leafNodes();
+    node.evaluate('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 });
+
+// test('minmax', () => {
+//     const node = new Node(3, 0, 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1', 'max');
+//     node.minmax();
+// })
+
+test('node evaluate boards with pruning', () => {
+    const node = new NodeWithPruning(4, 0, 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', 'max');
+    node.evaluate('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+});
+
+test('minmax with pruning', () => {
+    const node = new NodeWithPruning(3, 0, 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1', 'max');
+    node.minmaxab();
+})
--- src/operations/ai.ts
+++ src/operations/ai.ts
@@ -1,4 +1,5 @@
 import { Chess } from 'chess.js';
+import { PLAY_BLACK_SCORE, PLAY_WHITE_SCORE } from './constant';
 import shuffle from 'lodash/shuffle';
 
 export const stupidVersion = (fen: string) => {
@@ -32,20 +33,40 @@ export class Node {
         })
     }
 
-    evaluate() {
-        // TODO
-        return 0
+    evaluate(fen: string) {
+        const chessboard = new Chess(fen);
+        let score = 0;
+        chessboard.board().forEach((row: any[]) => {
+            row.forEach((piece: any) => {
+                if (piece) {
+                    if (piece.color === 'w') {
+                        score += PLAY_WHITE_SCORE[piece.type.toUpperCase()];
+                    } else {
+                        score += PLAY_BLACK_SCORE[piece.type];
+                    }
+                }
+            })
+        });
+        return score;
     }
-
+    switchType = (type: string) =>{
+        if (type === 'min') {
+            return 'max';
+        }
+        if (type === 'max') {
+            return 'min';
+        }
+    }
     minmax() {
-        if (this.targetDepth === this.depth) {
-            return this.evaluate();
+        if (this.depth >= this.targetDepth) {
+            return this.evaluate(this.fen);
         }
         if (this.type === 'max') {
+            // max node edit alpha
             let max = -Infinity;
-            for (let i = 0; i < this.leafNodes.length; i++) {
-                const childFen = this.leafNodes[i];
-                const child = new Node(this.targetDepth, this.depth + 1, childFen, 'min');
+            for (let i = 0; i < this.leafNodes().length; i++) {
+                const childFen = this.leafNodes()[i];
+                const child = new Node(this.targetDepth, this.depth + 1, childFen, this.switchType(this.type));
                 const value = child.minmax();
                 if (value > max) {
                     max = value;
@@ -54,10 +75,11 @@ export class Node {
             return max;
         }
         if (this.type === 'min') {
+            // min node edit beta
             let min = Infinity;
-            for (let i = 0; i < this.leafNodes.length; i++) {
-                const childFen = this.leafNodes[i];
-                const child = new Node(this.targetDepth, this.depth + 1, childFen, 'max');
+            for (let i = 0; i < this.leafNodes().length; i++) {
+                const childFen = this.leafNodes()[i];
+                const child = new Node(this.targetDepth, this.depth + 1, childFen, this.switchType(this.type));
                 const value = child.minmax();
                 if (value < min) {
                     min = value;
@@ -69,3 +91,57 @@ export class Node {
 
 }
 
+
+export class NodeWithPruning extends Node {
+    alpha: number;
+    beta: number;
+
+    constructor(
+        targetDepth: number, 
+        depth: number, 
+        fen: string, 
+        type: string, 
+        alpha: number, 
+        beta: number) {
+        super(targetDepth, depth, fen, type);
+        this.alpha = alpha || -Infinity;
+        this.beta = beta || Infinity;
+    }
+
+    minmaxab() {
+        if (this.depth >= this.targetDepth) {
+            return this.evaluate(this.fen);
+        }
+        if (this.type === 'max') {
+            // max node edit alpha
+            let max = -Infinity;
+
+            for (let i = 0; i < this.leafNodes().length; i++) {
+                const childFen = this.leafNodes()[i];
+                const child = new NodeWithPruning(this.targetDepth, this.depth + 1, childFen, this.switchType(this.type), this.alpha, this.beta);
+                const value = child.minmax();
+                this.alpha = Math.max(this.alpha, value);
+                if (this.alpha >= this.beta) {
+                    break;
+                }
+            }
+            return max;
+        }
+        if (this.type === 'min') {
+            // min node edit beta
+            let min = Infinity;
+
+            for (let i = 0; i < this.leafNodes().length; i++) {
+                const childFen = this.leafNodes()[i];
+                const child = new NodeWithPruning(this.targetDepth, this.depth + 1, childFen, this.switchType(this.type), this.alpha, this.beta);
+                const value = child.minmax();
+                this.beta = Math.min(this.beta, value);
+                if (this.alpha >= this.beta) {
+                    break;
+                }
+            }
+            return min;
+        }
+    }
+
+}
