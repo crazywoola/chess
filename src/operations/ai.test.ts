fd2d13256dc28c901cd123f9a0a0b36e1e489dbd
banana <banana@eurus.cn>
2022-04-03 18:32:09 +0800

    fix: rename to ts

--- src/operations/ai.test.ts
+++ src/operations/ai.test.ts
@@ -12,14 +12,14 @@ import {
 
 test('minmax with pruning', () => {
     const board = new Chess('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1');
-    const node = new ABNode(3, board, -Infinity, Infinity, true);
+    const node = new ABNode(4, board, -Infinity, Infinity, true);
     node.minmaxab();
     console.log(node.chosenMove);
 })
 
 test('negamax with pruning', () => {
     const board = new Chess('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1');
-    const node = new NegamaxNode(3, board, -Infinity, Infinity, true);
+    const node = new NegamaxNode(4, board, -Infinity, Infinity, true);
     node.negamax();
     console.log(node.chosenMove);
 })
